5ffb3f237e12c413564d4d483a7cb6db2e80d07d
wxnacy <371032668@qq.com>
2018-07-24 20:56:17 +0800

    add id module

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: wxnacy(wxnacy@gmail.com)
+# Description:
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'wpy',
+    version = '0.0.1',
+    keywords='python3',
+    description = 'a library for python Developer',
+    license = 'MIT License',
+    url = 'https://github.com/wxnacy/wpy',
+    author = 'wxnacy',
+    author_email = 'wxnacy@gmail.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [
+        'requests>=2.19.1'
+    ],
+)
+
