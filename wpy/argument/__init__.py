3e39db36b0bb50d65fb8fff0a63af95192a69ad7
wxnacy <371032668@qq.com>
2021-07-15 21:47:41 +0800

    U: 增加参数模块

---
+++ wpy/argument/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: wxnacy@gmail.com
+"""
+
+"""
+
+from .enum import Action
+from .parser import ArgumentParser
+from .parser import ArgumentNamespace
+
+
+__all__ = [
+    'Action',
+    'ArgumentParser',
+    'ArgumentNamespace',
+]
---
+++ wpy/argument/enum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: wxnacy@gmail.com
+"""
+枚举
+"""
+from enum import Enum
+
+class Action(Enum):
+    STORE = 'store'
+    STORE_TRUE = 'store_true'
+    APPEND = 'append'
+
