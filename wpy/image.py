dabc10526c31ec636082aa4928629a9c6c48db10
wxnacy <371032668@qq.com>
2021-12-03 12:57:09 +0800

    U: 去掉 image 专业工具

--- wpy/image.py
+++ wpy/image.py
@@ -4,26 +4,26 @@
 # Description:
 #   https://pillow.readthedocs.io/en/5.2.x/reference/Image.html#open-rotate-and-display-an-image-using-the-default-viewer
 
-from PIL import Image as I
-import io
-import requests
+#  from PIL import Image as I
+#  import io
+#  import requests
 
-class Image():
+#  class Image():
 
-    def __init__(self, *args, **kwargs):
-        self.path = args[0]
-        if self.path.startswith('http'):
-            self.is_network = True
-            res = requests.get(self.path)
-            self.data = res.content
-            self.image = I.open(io.BytesIO(self.data))
-        else:
-            self.is_local = True
-            self.image = I.open(self.path)
+    #  def __init__(self, *args, **kwargs):
+        #  self.path = args[0]
+        #  if self.path.startswith('http'):
+            #  self.is_network = True
+            #  res = requests.get(self.path)
+            #  self.data = res.content
+            #  self.image = I.open(io.BytesIO(self.data))
+        #  else:
+            #  self.is_local = True
+            #  self.image = I.open(self.path)
 
-        self.format = self.image.format.lower()
-        self.size = self.image.size
+        #  self.format = self.image.format.lower()
+        #  self.size = self.image.size
 
-    def save(self, path, size):
-        self.image.thumbnail(size)
-        self.image.save(path, self.format)
+    #  def save(self, path, size):
+        #  self.image.thumbnail(size)
+        #  self.image.save(path, self.format)
